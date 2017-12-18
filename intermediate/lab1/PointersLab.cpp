6a48c90a33d9c03ec0a855cc00ee290f31466e2f
monpeco <monpeco@gmail.com>
2017-12-18 17:31:23 +0000

    Add assign value 5 to memory location stored in pNum

--- intermediate/lab1/PointersLab.cpp
+++ intermediate/lab1/PointersLab.cpp
@@ -12,6 +12,10 @@ int main(){
   num1 = 3; // 3. Assign the value 3 to num1.
   if(debug) cout << num1 << endl;
   
+  // 4. Assign the value 5 to the memory location stored in pNum.
+  pNum = &num1;
+  *pNum = 5;
+  if(debug) cout << num1 << endl;
   
   return 0;
 }
