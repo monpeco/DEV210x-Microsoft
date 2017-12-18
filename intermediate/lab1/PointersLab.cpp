3f567584d115bad22c9d9f9e57ef20819b1be251
monpeco <monpeco@gmail.com>
2017-12-18 12:27:08 +0000

    Create pointers lab file

---
+++ intermediate/lab1/PointersLab.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  bool debug = true;
+  
+  int num1; // 2. In your main() function, create two variables.  One variable needs to an intrinsic integer variable named num1 and the second needs to be a pointer to an int pNum.
+  int *pNum;
+  
+  
+  num1 = 3; // 3. Assign the value 3 to num1.
+  if(debug) cout << num1 << endl;
+  
+  
+  return 0;
+}
