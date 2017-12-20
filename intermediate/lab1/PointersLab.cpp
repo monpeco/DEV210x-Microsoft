9f9bcc78d8afb1f6ce6634785f03c25235fee2fe
monpeco <monpeco@gmail.com>
2017-12-20 12:19:35 +0000

    Add PassByRef() and modify num1 value

--- intermediate/lab1/PointersLab.cpp
+++ intermediate/lab1/PointersLab.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void PassByValue(int parameter);
+void PassByRef(int *parameter);
 
 int main(){
   bool debug = true;
@@ -22,12 +23,28 @@ int main(){
   // 7. Call PassByValue and pass in num1 as the argument.
   PassByValue(num1);
   
-  // 8. Modify the value of num1 inside the PassByValue function and output the new value to the console window.
+  // 9. After the call returns to main(), output the value of num1 to the console window again.
   cout << "After PassByValue return to main: " << num1 << endl;
+  
+  // 11. Call PassByRef and send pNum as the argument.
+  PassByRef(pNum);
+  
+  // 14. After the call returns to main(), output the value of pNum to the console.
+  cout << "After PassByRef return to main: " << num1 << endl;
   
   return 0;
 }
 
+// 10. Create a function called PassByRef that accepts a reference variable as an argument
+void PassByRef(int *parameter){
+  // 12. Output a statement indicating the current function name.
+  cout << "PassByRef" << endl;
+  
+  // 13. Modify the value of pNum, setting it to 50,  and output the value to the console.
+  *parameter = 50;
+  cout << "Inside PassByRef: " << *parameter << endl;
+  
+}
 
 // 5. Create a function called PassByValue that accepts an integer argument.
 void PassByValue(int parameter){
@@ -40,3 +57,4 @@ void PassByValue(int parameter){
   cout << "Inside PassByValue: " << parameter << endl;
   
 }
+
