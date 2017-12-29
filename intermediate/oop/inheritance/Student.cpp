2905bdf46f45bf73c99909b0acf9ebbc1518b860
monpeco <monpeco@gmail.com>
2017-12-29 13:17:41 +0000

    Add getter and setter for protected age

--- intermediate/oop/inheritance/Student.cpp
+++ intermediate/oop/inheritance/Student.cpp
@@ -12,3 +12,25 @@ Student::Student():Person("Tom", "Thumb"){
 Student::~Student(){
   cout << "    Student::~Student()" << endl;
 }
+
+void Student::setAge(int age)
+{
+    if (age < 5)
+    {
+        cout << "Student age needs to at least 5 years old." << endl;
+    }
+    else
+    {
+       this->age = age;
+    }
+}
+
+int Student::getAge()
+{
+    return this->age;
+}
+
+void Student::sayHello()
+{
+    cout << "Hey, how's it goin'?" << endl;
+}
--- intermediate/oop/inheritance/Student.h
+++ intermediate/oop/inheritance/Student.h
@@ -6,4 +6,8 @@ class Student : public Person{
   public:
     Student();
     ~Student();
+    
+    void setAge(int age);
+    int getAge();
+    void sayHello();
 };
