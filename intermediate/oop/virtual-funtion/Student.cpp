53a5c637287ac8a8ae0b004bce72b37e46928583
monpeco <monpeco@gmail.com>
2018-01-03 21:24:08 +0000

    Add call the base class version of the virtual function

--- intermediate/oop/virtual-funtion/Student.cpp
+++ intermediate/oop/virtual-funtion/Student.cpp
@@ -1,5 +1,6 @@
 #include "Student.h"
 
 void Student::display() const{
+  Person::display();  // Call the base class version of display()
   cout << "Override Person::display() in Student Implementation" << endl;
 }
