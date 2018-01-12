ed2b44088acf84b90d226e97130b59a78a5cf813
monpeco <monpeco@gmail.com>
2018-01-12 03:37:31 +0000

    Add student constructors

---
+++ intermediate/oop/lab3/student.cpp
@@ -0,0 +1,14 @@
+#include "student.h"
+
+Student::Student(){
+  
+}
+
+Student::Student(string first_name, string last_name, int age, string race)
+: Person(first_name, last_name, age, race){
+
+}
+
+Student::~Student(){
+  
+}
--- intermediate/oop/lab3/student.h
+++ intermediate/oop/lab3/student.h
@@ -4,5 +4,7 @@ class Student : public Person{
   private:
   
   public:
-  
+    Student();
+    Student(string first_name, string last_name, int age, string race);
+    ~Student();
 };
