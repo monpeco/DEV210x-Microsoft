9f6eacfd29ce9bd6021d592bf049a6ee4f4c4580
monpeco <monpeco@gmail.com>
2018-01-13 14:41:03 +0000

    Improve output and call teacher outputAge

--- intermediate/oop/lab3/main.cpp
+++ intermediate/oop/lab3/main.cpp
@@ -7,17 +7,24 @@ using namespace std;
 
 int main(){
   
+  cout << "Create person1" << endl;
+
   Person person1;
   person1.outputIdentity();
   person1.outputAge();
   
+  cout << "\nCreate student1" << endl;
+
   Student student1("string first_name", "string last_name", 12, "string race");
   student1.outputIdentity();
   student1.outputAge();
 
+  cout << "\nCreate teacher1" << endl;
+
   Teacher teacher1;
+  teacher1.outputIdentity();
   teacher1.outputAge();
-  
+
   /*g++ main.cpp person.cpp student.cpp teacher.cpp -o main.exe*/
 
   return 0;
