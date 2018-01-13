8a10972737e03f2511c5b581352a8692f4a75ccd
monpeco <monpeco@gmail.com>
2018-01-13 15:00:10 +0000

    Add Teacher's constructors and destructor

---
+++ intermediate/oop/lab3/teacher.cpp
@@ -0,0 +1,14 @@
+#include "teacher.h"
+
+Teacher::Teacher(){
+  
+}
+
+Teacher::Teacher(string first_name, string last_name, int age, string race)
+: Person(first_name, last_name, age, race){
+  
+}
+
+Teacher::~Teacher(){
+  
+}
--- intermediate/oop/lab3/teacher.h
+++ intermediate/oop/lab3/teacher.h
@@ -4,5 +4,7 @@ class Teacher : public Person{
   private:
   
   public:
-  
+    Teacher();
+    Teacher(string first_name, string last_name, int age, string race);
+    ~Teacher();
 };
